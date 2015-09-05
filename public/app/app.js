4c4dbc356588061f285e02f1fc54d71baf700587
Sri Aurobindo Munagala <koolschoolfan@gmail.com>
2015-09-05 11:52:27 +0530

    Shits never working

--- public/app/app.js
+++ public/app/app.js
@@ -5,12 +5,13 @@ angular.module('myApp', [
   'ipCookie',
   'ngRoute',
   'ng-token-auth',
+  'ui-compat',
   'myApp.signin',
   'myApp.signup',
   'myApp.version',
   'myApp.user_index'
 ]).
-config(['$routeProvider', function($routeProvider){
+config([ '$stateProvider', '$routeProvider', function( $routeProvider, $stateProvider){
       $routeProvider
       .when('/sign_in', {
         templateUrl: 'signin/signin.html',
@@ -27,7 +28,35 @@ config(['$routeProvider', function($routeProvider){
       .otherwise({
         redirectTo: '/'
       });
-}]).
+
+        $stateProvider
+            // this state will be visible to everyone
+            .state('index', {
+                url: '/',
+                templateUrl: 'index.html',
+                controller: 'IndexCtrl'
+            })
+
+            // only authenticated users will be able to see routes that are
+            // children of this state
+            .state('user_index', {
+                url: '/user_index',
+                abstract: true,
+                template: '<ui-view/>',
+                resolve: {
+                    auth: function($auth) {
+                        return $auth.validateUser();
+                    }
+                }
+            })
+
+        // this route will only be available to authenticated users
+        /*.state('admin.dashboard', {
+         url: '/dash',
+         templateUrl: '/admin/dash.html',
+         controller: 'AdminDashCtrl'
+         });*/
+}, ]).
 run(['$rootScope', '$location', function($rootScope, $location) {
     $rootScope.$on('auth:login-success', function() {
         console.log($rootScope.user)
