59259db28ad5892b362c3f3e76c68e12ff8b7247
mukulhase <mukulhase@gmail.com>
2015-09-05 15:47:01 +0530

    angular resources installed

--- public/app/app.js
+++ public/app/app.js
@@ -5,59 +5,31 @@ angular.module('myApp', [
   'ipCookie',
   'ngRoute',
   'ng-token-auth',
-  'ui-compat',
+  'ngResource',
   'myApp.signin',
   'myApp.signup',
   'myApp.version',
   'myApp.user_index'
 ]).
-config([ '$stateProvider', '$routeProvider', function( $routeProvider, $stateProvider){
-      $routeProvider
-      .when('/sign_in', {
-        templateUrl: 'signin/signin.html',
-        controller: 'Signin'
-      })
-      .when('/sign_up', {
-          templateUrl: 'signup/signup.html',
-          controller: 'Signup'
-      })
-      .when('/user_index', {
-          templateUrl: 'user_index/user_index.html',
-          controller: 'UserIndex'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-        $stateProvider
-            // this state will be visible to everyone
-            .state('index', {
-                url: '/',
-                templateUrl: 'index.html',
-                controller: 'IndexCtrl'
+config(['$routeProvider', function( $routeProvider, $stateProvider) {
+        $routeProvider
+            .when('/sign_in', {
+                templateUrl: 'signin/signin.html',
+                controller: 'Signin'
             })
-
-            // only authenticated users will be able to see routes that are
-            // children of this state
-            .state('user_index', {
-                url: '/user_index',
-                abstract: true,
-                template: '<ui-view/>',
-                resolve: {
-                    auth: function($auth) {
-                        return $auth.validateUser();
-                    }
-                }
+            .when('/sign_up', {
+                templateUrl: 'signup/signup.html',
+                controller: 'Signup'
             })
-
-        // this route will only be available to authenticated users
-        /*.state('admin.dashboard', {
-         url: '/dash',
-         templateUrl: '/admin/dash.html',
-         controller: 'AdminDashCtrl'
-         });*/
-}, ]).
-run(['$rootScope', '$location', function($rootScope, $location) {
+            .when('/user_index', {
+                templateUrl: 'user_index/user_index.html',
+                controller: 'UserIndex'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]).
+    run(['$rootScope', '$location', function($rootScope, $location) {
     $rootScope.$on('auth:login-success', function() {
         console.log($rootScope.user)
         $location.path('/');
