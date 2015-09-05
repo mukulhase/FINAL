146095a66d99513790f6b28a12db3b97b848b84c
mukulhase <mukulhase@gmail.com>
2015-09-05 21:43:13 +0530

    add new project inc

--- public/app/user_index/user_index.js
+++ public/app/user_index/user_index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('myApp.user_index', ['ngRoute'])
-.controller('UserIndex', ['$scope', '$auth', '$resource', function ($scope, $auth, $resource) {
-        var User = $resource('/user/:userId', {userId:'@id'});
-        var user = User.get({userId:0}, function() {
-            $scope.A1=user.Name;
+.controller('UserIndex', ['$scope', '$rootScope', '$auth', '$resource', function ($scope, $rootScope, $auth, $resource) {
+        $scope.addProject = function() {
+            var Project = $resource('/projects');
+            var newproject = new Project({owner_id:$rootScope.user.id,Name:$scope.newprojectname});
+            newproject.$save();
+        }
 
-            user.$save();
-        });
 }]);
