d56d39abe1c19b717e9c8253a3bb9d7555e681f0
mukulhase <mukulhase@gmail.com>
2015-09-12 15:37:56 +0530

    only display that users shit

--- public/app/user_index/user_index.js
+++ public/app/user_index/user_index.js
@@ -4,9 +4,12 @@ angular.module('myApp.user_index', ['ngRoute'])
 .controller('UserIndex', ['$http', '$scope', '$rootScope', '$auth', '$resource', function ($http, $scope, $rootScope, $auth, $resource) {
         $scope.projects=[];
         var Project = $resource('/projects');
-
+        if(!$rootScope.user.id){
+            location.reload();
+        }
+        var Users_Projects =$resource('/user/:id.json',{id:$rootScope.user.id});
         function refresh(){
-            Project.query(function(data){
+            Users_Projects.query(function(data){
                 console.log($scope.projects);
                 for(var i=0;i<data.length;i++){
                     var flag=0;
@@ -18,6 +21,18 @@ angular.module('myApp.user_index', ['ngRoute'])
                     }
                     if(flag==0){
                         $scope.projects.push(data[i]);
+                    }
+                }
+                for(var i=0;i<$scope.projects.length;i++){
+                    var flag=0;
+                    for (var j=0;j<data.length;j++){
+                        if($scope.projects[j].id==data[i].id){
+                            flag++;
+                            break;
+                        }
+                    }
+                    if(flag==0){
+                        $scope.projects.splice(i,1);
                     }
                 }
             });
@@ -33,10 +48,11 @@ angular.module('myApp.user_index', ['ngRoute'])
             var project = ProjectInstance.get({ID:id.id}, function() {
                 project.$delete();
             });
-            $scope.projects.splice(
+            refresh();
+            /*$scope.projects.splice(
                 index,
                 1
-            );
+            );*/
         };
         $scope.addProject = function() {
             var newproject = new Project({owner_id:$rootScope.user.id,Name:$scope.newprojectname});
